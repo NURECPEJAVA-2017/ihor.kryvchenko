2a6b9911e0a8ce1aabb47a9df6544702f3548279
Igor Krivchenko <34036485+Krivchenko1306@users.noreply.github.com>
2017-12-01 07:32:36 +0200

    Update UserTest.java

--- UserManagment/src/test/java/ua/nure/krivchenko/UserTest.java
+++ UserManagment/src/test/java/ua/nure/krivchenko/UserTest.java
@@ -6,29 +6,6 @@ import org.junit.Test;
 
 public class UserTest extends TestCase {
 
-    public Integer lessNull(Integer x)
-    {
-        if(x < 0)
-            return 0;
-        else
-            return 1;
-    }
-
-    public Integer checkNull(Integer x)
-    {
-        if(x == 0)
-            return 1;
-        else
-            return 0;
-    }
-
-    public Integer checkExcess(Integer x){
-        if(x > 120)
-            return 0;
-        else
-            return 1;
-    }
-
     @Test
     public void testFullName() {
         User user = new User();
@@ -39,15 +16,6 @@ public class UserTest extends TestCase {
     }
     @Test
     public void testAge(){
-        Integer expectedLessNull = 1;
-        Integer expectedCheckNull = 0;
-        Integer expectedCheckExcess = 1;
-        User user = new User();
-        user.setAge(100);
-        Integer receivedAge = user.getAge();
-        assertEquals(expectedLessNull,lessNull(receivedAge)); // will pass the test if age > 0
-        assertEquals(expectedCheckNull,checkNull(receivedAge)); // will pass the test if age != 0
-        assertEquals(expectedCheckExcess,checkExcess(receivedAge)); //will pas the test if age < 120
 
     }
 
