e2fe4933fdca175a03b98159cd736cfab83feecb
Krivchenko1306 <ihor.kryvchenko.cpe@nure.ua>
2017-12-05 23:20:23 +0200

    test commit 2

--- UserManagment/src/main/java/ua/nure/krivchenko/User.java
+++ UserManagment/src/main/java/ua/nure/krivchenko/User.java
@@ -1,5 +1,6 @@
 package ua.nure.krivchenko;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class User {
@@ -39,6 +40,25 @@ public class User {
 
     public String getFullName(){
         return new StringBuilder().append(firstName).append(" ").append(lastName).toString();
+    }
+
+    public int getAge(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        int currentDay = calendar.get(Calendar.DAY_OF_MONTH);
+        int currentMonth = calendar.get(Calendar.MONTH);
+        int currentYear = calendar.get(Calendar.YEAR);
+        calendar.setTime(getDateOfBirth());
+
+        int day = calendar.get(calendar.DAY_OF_MONTH);
+        int month = calendar.get(calendar.MONTH);
+        int year = calendar.get(Calendar.YEAR);
+
+        //(currentMonth < month)
+
+        int recivedYear = currentYear - year - 1;
+        return recivedYear;
+
     }
 
 }
